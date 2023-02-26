e6f25166074d1e9afffafb3c16f1ea1ea730712d
Martyn Pratt <martyni@users.noreply.github.com>
2023-02-26 20:30:02 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+with open('requirements.txt') as requirements:
+   setup(
+      name		= 'status',
+      version		= '3.7',
+      packages 		= ['status'],
+      package_dir	= {'': '.'},
+      author		= 'askmartyn',
+      description	= 'Simple service that reads json status file',
+      install_requires	= [ req for req in requirements.readlines()]
+   )
